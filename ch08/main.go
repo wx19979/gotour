bdd78906a5e33256cfa85c269f095c18df2c4c8e
wangxu <wxmygit@protonmail.com>
2024-07-08 14:29:36 +0800

    message ch08 ch09

--- ch08/main.go
+++ ch08/main.go
@@ -1,3 +1,4 @@
+// 通道的测试代码
 package main
 
 import (
@@ -6,27 +7,31 @@ import (
 )
 
 func main() {
+	//创建一个通道对象
 	ch := make(chan string)
-
+	//执行一个go的协程将信息放入通道之中
 	go func() {
 		fmt.Println("飞雪无情")
 		ch <- "goroutine 完成"
 	}()
-
+	//打印结果输出当前是主函数的协程信息
 	fmt.Println("我是 main goroutine")
-
+	//将通道ch的信息放到变量v中
 	v := <-ch
-	fmt.Println("接收到的chan中的值为：", v)
-
+	//输出具体的信息
+	fmt.Println("接收到的chan中的值为:", v)
+	//创建一个缓冲区大小是5的一个通道
 	cacheCh := make(chan int, 5)
+	//放入两个数据
 	cacheCh <- 2
 	cacheCh <- 3
+	//输出该通道的信息
 	fmt.Println("cacheCh容量为:", cap(cacheCh), ",元素个数为：", len(cacheCh))
-
+	//创建三个string类型的通道
 	firstCh := make(chan string)
 	secondCh := make(chan string)
 	threeCh := make(chan string)
-
+	//三个协程分被执行下载文件操作
 	go func() {
 		firstCh <- downloadFile("firstCh")
 	}()
@@ -38,17 +43,18 @@ func main() {
 	go func() {
 		threeCh <- downloadFile("threeCh")
 	}()
-
+	//通过select语句实现并发
 	select {
-	case filePath := <-firstCh:
+	case filePath := <-firstCh: //当为第一路径时输出内容
 		fmt.Println(filePath)
-	case filePath := <-secondCh:
+	case filePath := <-secondCh: //当为第二路径时输出内容
 		fmt.Println(filePath)
-	case filePath := <-threeCh:
+	case filePath := <-threeCh: //当为第三路径时输出内容
 		fmt.Println(filePath)
 	}
 }
 
+// 模拟下载环境的函数
 func downloadFile(chanName string) string {
 	//模拟下载文件,可以自己随机time.Sleep点时间试试
 	time.Sleep(time.Second)
--- ch09/main.go
+++ ch09/main.go
@@ -18,18 +18,19 @@ func main() {
 	syncMap()
 }
 
+// 模拟跑步场景的函数
 func run() {
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup //创建等待信号量
 	//因为要监控110个协程，所以设置计数器为110
 	wg.Add(110)
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 100; i++ { //逐次的创建协程
 		go func() {
 			//计数器值减1
 			defer wg.Done()
 			add(10)
 		}()
 	}
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 10; i++ { //循环操作进行相加操作
 		go func() {
 			//计数器值减1
 			defer wg.Done()
@@ -40,25 +41,28 @@ func run() {
 	wg.Wait()
 }
 
+// 单例操作
 func doOnce() {
+	//创建只执行一次的信号量
 	var once sync.Once
-	onceBody := func() {
+	onceBody := func() { //创建一次执行函数
 		fmt.Println("Only once")
 	}
-	done := make(chan bool)
-	for i := 0; i < 10; i++ {
-		go func() {
+	done := make(chan bool)   //创建一个通道
+	for i := 0; i < 10; i++ { //模拟执行10次
+		go func() { //创建协程调用执行函数
 			once.Do(onceBody)
-			done <- true
+			done <- true //修改done的通道值为true
 		}()
 	}
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 10; i++ { //依次输出done通道内容
 		<-done
 	}
 }
 
-//10个人赛跑,1个裁判发号施令
+// 10个人赛跑,1个裁判发号施令
 func race() {
+	//创建加锁的信号量
 	cond := sync.NewCond(&sync.Mutex{})
 	var wg sync.WaitGroup
 	wg.Add(11)

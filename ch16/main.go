04d26fd7b5a8cb8de2a5b41695d2a57394758a24
wx19979 <wxmygit@protonmail.com>
2024-07-11 21:12:53 +0800

    message ch16 ch17

--- ch16/main.go
+++ ch16/main.go
@@ -1,3 +1,23 @@
+// 反射是计算机语言中程序检视自身结构的一种方法,属于元编程的一种形式
+// 反射三大定律:(1)任何接口值interface{}都可反射出反射对象
+// (2)反射对象也可还原为interface{}变量(3)要修改反射对象,该值必须可设置,可寻址
+// 字符串和结构体之间的转换是通过运行时反射实现的
+// Go语言反射定义中,任何接口都由两部分组成:
+// (1)接口的具体类型(2)具体类型对应的值
+// (1)reflect.Value(本质是一个结构体) (2)reflect.Type(本质是一个接口)
+// 其中interface{}是空接口,可以表示任何类型，可以把任何类型
+// 转换为空接口,常用于反射、类型断言以减少代码重复,简化编程
+// reflect.Value对应的函数类别:(1)获取或修改对应的值(2)和struct类型字段
+// 有关用于获取对应字段(3)和类型上方法集有关用于获取对应方法
+// 修改对应的值的步骤:(1)传递struct结构体指针对应的值reflect.Value
+// (2)通过Elem方法获取指针指向的值(3)通过Field方法获取要修改的字段
+// (4)通过Set系列方法修改成对应的值
+// reflect.Type几个特殊的函数:(1)Implements方法用于判断是否实现了接口u
+// (2)AssignableTo方法用于判断是否可以赋值给类型u(3)ConertibleTo方法用于
+// 判读是否可以转换为类型u(4)Comparable方法用于判断该类型是否是可比较的
+// 字符串和结构体互转场景最多的就是JSON和struct的互转
+// struct tag是添加在struct字段上的标记,用它进行辅助,可以完成一些额外操作
+// 如JSON和struct互转
 package main
 
 import (
@@ -26,7 +46,7 @@ func main() {
 	ppv := reflect.ValueOf(&p)          //获取到人的指针
 	ppv.Elem().Field(0).SetString("张三") //设置这个人的指针对应第一个参数的值为张三
 	fmt.Println(p)
-	fmt.Println("ppv ", ppv.Kind()) //输出ppv的类型为指针
+	fmt.Println("ppv ", ppv.Kind()) //输出ppv的类型为指针ppv.Kind()是直接输出其对应的底层类型
 
 	pv := reflect.ValueOf(p) //输出当前反射的值
 	fmt.Println(pv.Kind())   //输出当前pv的类型
@@ -43,18 +63,18 @@ func main() {
 	//创建一个stringerType和一个writerType
 	stringerType := reflect.TypeOf((*fmt.Stringer)(nil)).Elem()
 	writerType := reflect.TypeOf((*io.Writer)(nil)).Elem()
-	fmt.Println("是否实现了fmt.Stringer：", pt.Implements(stringerType))
+	fmt.Println("是否实现了fmt.Stringer：", pt.Implements(stringerType)) //判断是否实现了该接口
 	fmt.Println("是否实现了io.Writer：", pt.Implements(writerType))
 
 	//struct to json
-	jsonB, err := json.Marshal(p)
+	jsonB, err := json.Marshal(p) //结构体转换为字符串
 	if err == nil {
 		fmt.Println(string(jsonB))
 	}
 
 	//json to struct
 	respJSON := "{\"name\":\"李四\",\"age\":40}"
-	json.Unmarshal([]byte(respJSON), &p)
+	json.Unmarshal([]byte(respJSON), &p) //字符串转换为结构体
 	fmt.Println(p)
 
 	//遍历person字段中key为json、bson的tag
--- ch17/main.go
+++ ch17/main.go
@@ -1,3 +1,15 @@
+// unsafe包,可绕过Go的安全机制,直接对内存进行读写
+// unsafe.Pointer:一种特殊意义的指针,可以表示任意类型的地址
+// 类似C语言里面的void*指针,全能型的,但是该指针不能进行指针运算
+// uintptr:该类型可以对指针进行偏移运算,这样就可以访问特定内存,
+// 达到对特定内存进行读写的目的,这是真正内存级别的操作
+// 共有*T,unsafe.Pointer,uintptr三种类型的指针
+// 指针转换规则:任何类型*T都可转换为unsafe.Pointer,unsafe.Pointer
+// 同样也可以转换为任何类型*T,uintptr可转为unsafe.Pointer,同样
+// unsafe.Pointer也可转为uintptr类型unsafe.Pointer是一个桥梁
+// Sizeof函数可返回一个类型所占用的内存大小,大小与类型有关,和类型
+// 对应变量存储内容大小无关
+
 package main
 
 import (
@@ -18,7 +30,7 @@ func main() {
 	pName := (*string)(unsafe.Pointer(p))
 	*pName = "飞雪无情"
 	//Age并不是person的第一个字段，所以需要进行偏移，这样才能正确定位到Age字段这块内存，才可以正确的修改
-	pAge := (*int)(unsafe.Pointer(uintptr(unsafe.Pointer(p)) + unsafe.Offsetof(p.Age)))
+	pAge := (*int)(unsafe.Pointer(uintptr(unsafe.Pointer(p)) + unsafe.Offsetof(p.Age))) //是通过指针偏移找到对应的值的操作
 	*pAge = 20
 
 	fmt.Println(*p)
